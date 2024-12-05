eec3cf11de2bfe3ad2a034742dceb1243b3e4148
alinamdari79 <90065028+alinamdari79@users.noreply.github.com>
2024-12-06 01:04:57 +0330

    create onboarding page

---
+++ src/pages/onboarding/onboarding.tsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+const Onboarding = () => {
+  const [] = useState();
+
+  return (
+    <>
+      <p className="font-bold flex justify-center">aliiiii</p>
+    </>
+  );
+};
+export default Onboarding;
